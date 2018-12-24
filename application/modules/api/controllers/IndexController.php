c91a3f87e95d2f627802985285db88f9b21698bb
samlanh <mok_channy@yahoo.com>
2018-12-24 09:02:43 +0700

    commit api

---
+++ application/modules/api/controllers/IndexController.php
@@ -0,0 +1,31 @@
+<?php
+class Api_IndexController extends Zend_Controller_Action {
+	
+// 	const REDIRECT_URL='/project';
+	protected $tr;
+	public function init()
+	{
+		$this->tr=Application_Form_FrmLanguages::getCurrentlanguage();
+		/* Initialize action controller here */
+		header('content-type: text/html; charset=utf8');
+		defined('BASE_URL')	|| define('BASE_URL', Zend_Controller_Front::getInstance()->getBaseUrl());
+	}
+	public function indexAction(){
+  		$this->_helper->layout()->disableLayout();
+		$db = new Api_Model_DbTable_Dbapi();
+		print_r(Zend_Json::encode($db->getAllSold()));
+		exit();
+	}
+	public function incomeAction(){
+		$this->_helper->layout()->disableLayout();
+		$db = new Api_Model_DbTable_Dbapi();
+		print_r(Zend_Json::encode($db->getAllIncome()));
+		exit();
+	}
+	function expenseAction(){
+		$db = new Api_Model_DbTable_Dbapi();
+		print_r(Zend_Json::encode($db->getExpenseType()));
+		exit();
+	}
+}
+
---
+++ application/modules/api/models/DbTable/Dbapi.php
@@ -0,0 +1,37 @@
+<?php
+
+class Api_Model_DbTable_Dbapi extends Zend_Db_Table_Abstract
+{
+
+    protected $_name = 'ln_properties';
+    public function getUserId(){
+    	$session_user=new Zend_Session_Namespace('authinstall');
+    	return $session_user->user_id;
+    }
+    function getAllSold(){
+    	$sql="SELECT * ,
+      	 (SELECT first_name FROM `rms_users` WHERE id=v_soldreport.user_id LIMIT 1) AS user_name,
+      	 (SELECT name_kh FROM `ln_view` WHERE key_code =v_soldreport.payment_id AND type = 25 limit 1) AS paymenttype
+      	 FROM v_soldreport WHERE 1 ORDER BY id DESC ";
+    	$db = $this->getAdapter();
+    	return $db->fetchAll($sql);
+    }
+    function getAllIncome(){
+    	$sql="SELECT *,
+			(SELECT first_name FROM `rms_users` WHERE id=v_getcollectmoney.user_id LIMIT 1) AS user_name,
+			(SELECT s.price_sold FROM `ln_sale` AS s WHERE s.id = sale_id LIMIT 1) AS sold_price
+      	FROM v_getcollectmoney WHERE status=1 ";
+    	$sql.= " ORDER BY id DESC ";
+    	$db = $this->getAdapter();
+    	return $db->fetchAll($sql);
+    }
+    function getExpenseType(){
+    	$db = $this->getAdapter();
+    	$sql="SELECT id,
+    		(SELECT project_name FROM `ln_project` WHERE ln_project.br_id =branch_id LIMIT 1) AS branch_name,
+    		(SELECT name_kh FROM `ln_view` WHERE type=13 and key_code=category_id limit 1) AS category_name,
+    		 SUM(total_amount) AS total_amount
+    		FROM ln_expense WHERE status=1 AND total_amount>0 group by category_id order by date desc";
+    	return $db->fetchAll($sql);
+    }
+}
