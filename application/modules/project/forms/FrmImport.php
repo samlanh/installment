7d9423d49f1465b00110f9b880cd91bdcf79b1de
samlanh <mok_channy@yahoo.com>
2019-11-01 14:23:45 +0700

    DD Import Property With Branch

---
+++ application/modules/project/forms/FrmImport.php
@@ -0,0 +1,38 @@
+<?php
+
+class Project_Form_FrmImport extends Zend_Dojo_Form
+{
+	protected  $tr;
+	protected $filter;
+
+    public function init()
+    {
+    	$this->tr=Application_Form_FrmLanguages::getCurrentlanguage();	
+    	$this->filter = 'dijit.form.FilteringSelect';
+    }
+    function FrmImport($data){
+    	$request=Zend_Controller_Front::getInstance()->getRequest();
+    	$typeItems = empty($typeItems)?1:$typeItems;
+    	$_dbgb = new Application_Model_DbTable_DbGlobal();
+    	
+   	 	$branch_id = new Zend_Dojo_Form_Element_FilteringSelect('branch_id');
+		$branch_id->setAttribs(array(
+				'dojoType'=>'dijit.form.FilteringSelect',
+				'class'=>'fullside',
+				'required' =>'true',
+				'autoComplete'=>'false',
+				'queryExpr'=>'*${0}*',
+		));
+		$options_branch = $_dbgb->getAllBranchName(null,1);
+		$branch_id->setMultiOptions($options_branch);
+    	
+    	
+    	if(!empty($data)){
+    		$branch_id->setValue($data["branch_id"]);
+    	}
+    	$this->addElements(array(
+    			$branch_id,
+    			));
+    	return $this;
+    }
+}
---
+++ application/modules/project/models/DbTable/DbImport.php
@@ -0,0 +1,60 @@
+<?php
+
+class Project_Model_DbTable_DbImport extends Zend_Db_Table_Abstract
+{
+
+    protected $_name = 'ldc_product';
+    public function getUserId(){
+    	$session_user=new Zend_Session_Namespace('auth');
+    	return $session_user->user_id;
+    
+    }
+    
+    function checkItemsTypeId($category){
+    	$db = $this->getAdapter();
+    	$sql="SELECT d.* FROM ln_properties_type AS d WHERE d.type_nameen = '$category' LIMIT 1";
+    	return $db->fetchRow($sql);
+    }
+    
+    public function updateItemsByImport($data,$branch_id){
+    	$db = $this->getAdapter();
+    	$count = count($data);
+    	$dbg = new Application_Model_DbTable_DbGlobal();
+    	for($i=1; $i<=$count; $i++){
+    		$land_code = $dbg->getNewLandByBranch($branch_id);
+    		
+//     		$cate_title = empty($data[$i]['D'])?null:$data[$i]['D'];
+//     		$cate = $this->checkItemsTypeId($cate_title);
+//     		$property_type = 0;
+//     		if (!empty($cate['id'])){
+//     			$property_type = $cate['id'];
+//     		}
+    		$_arr=array(
+    				'branch_id'	  => $branch_id,
+    				'land_code'	  => $land_code,
+    				'property_type'		=> 2,
+    				'land_address'			=> $data[$i]['B'],
+    				'street'    		=> $data[$i]['C'],
+    				
+    				'land_price'  => empty($data[$i]['E'])?0:$data[$i]['E'],
+    				'house_price' => 0,
+    				'price'	  => empty($data[$i]['E'])?0:$data[$i]['E'],
+    				
+    				'land_size'	  => $data[$i]['H'],
+					'width'       => $data[$i]['G'],
+					'height'      => $data[$i]['F'],
+    				
+    				'north'       => $data[$i]['I'],
+    				'south'	  => $data[$i]['J'],
+    				'west'      => $data[$i]['K'],
+    				'east'      => $data[$i]['L'],
+    				
+    				'create_date' 	=> date("Y-m-d H:i:s"),
+    				'user_id'	 	=> $this->getUserId()
+    		);
+    		$this->_name = "ln_properties";
+    		$pro_id =  $this->insert($_arr);
+    	}
+    }
+}   
+
