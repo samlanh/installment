29863b8e8a4defdf5b824446a1831b5c97e2eac8
samlanh <mok_channy@yahoo.com>
2021-07-05 10:52:19 +0700

    DD

--- application/models/DbTable/DbUserLog.php
+++ application/models/DbTable/DbUserLog.php
@@ -6,21 +6,25 @@ class Application_Model_DbTable_DbUserLog extends Zend_Db_Table_Abstract
     protected $_name = 'rms_user_log';
 
 	public function insertLogin($user_id){
-    	$data=array(
+    	/*
+		$data=array(
     					'log_date'=>date("Y/m/d , H:i:s"),
     					'log_type'=>'in',
     					'user_id'=>$user_id	
     	           );
     	 $this->insert($data);
+		*/
     }
     public function insertLogout($user_id)
     {
+		/*
     	$data=array(
     					'log_date'=>date("Y/m/d , H:i:s"),
     					'log_type'=>'out',
     					'user_id'=>$user_id	
     	           );    	 
     	 $this->insert($data);
+		 */
     }
     public static function writeMessageError($err)
     {
